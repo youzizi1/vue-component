604990bdcedb01daa309d33d7a4fe42556498f7c
ugu <yinyun957@gmail.com>
2019-12-03 22:22:41 +0800

    init button component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <u-button></u-button>
+    <u-button>11</u-button>
   </div>
 </template>
 
--- src/components/UButton.vue
+++ src/components/UButton.vue
@@ -1,3 +1,22 @@
 <template>
-  <button>hello</button>
+  <button :class="$style.button">
+    <i class="vcfont u-settings"></i>
+    <slot></slot>
+  </button>
 </template>
+
+<style lang="scss" module>
+@import "../assets/style/button.scss";
+
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 78px;
+  height: 32px;
+  border: 1px solid #dcdfe6;
+  background-color: #fff;
+  font-size: 14px;
+  cursor: pointer;
+}
+</style>
