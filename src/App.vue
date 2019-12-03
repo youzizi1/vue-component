391e864706ce23b6591c3602b6b3e5cbd2893711
ugu <yinyun957@gmail.com>
2019-12-03 15:22:13 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <u-button></u-button>
+  </div>
+</template>
+
+<script>
+import UButton from "./components/UButton.vue";
+
+export default {
+  components: {
+    UButton
+  }
+};
+</script>
