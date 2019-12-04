ac284d935d236bd5bec543094698b0484ed9fe9f
ugu <yinyun957@gmail.com>
2019-12-04 17:41:51 +0800

    add htmlwebpackplugin

--- config/config.dev.js
+++ config/config.dev.js
@@ -2,11 +2,16 @@ const path = require('path')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const baseWebpackConfig = require('./config.base')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(baseWebpackConfig, {
   mode: 'development',
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.html'),
+      filename: 'index.html'
+    })
   ],
   devServer: {
     contentBase: path.resolve(__dirname, '../dist'),
